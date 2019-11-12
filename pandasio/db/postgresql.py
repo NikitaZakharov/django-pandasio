1a39f189ebf7334fd1def3915181b519144e0409
NikitaZakharov <jake2000nik@gmail.com>
2019-11-12 16:58:21 +0200

    fixed validated_data property in case serializer is not valid and added DO NOTHING for upsert

--- pandasio/db/postgresql.py
+++ pandasio/db/postgresql.py
@@ -17,17 +17,19 @@ class DataFrameDatabaseSaver(BaseDataFrameDatabaseSaver):
             self.upsert(dataframe=dataframe, model=model)
 
     def upsert(self, dataframe, model):
+        upsert_clause = get_upsert_clause_sql(model)
+        do_statement = 'DO UPDATE SET %s ' % upsert_clause if upsert_clause else 'DO NOTHING '
         self._cursor.execute(
             'INSERT INTO %(table)s (%(columns)s) '
             'VALUES %(values)s '
             'ON CONFLICT (%(unique_columns)s) '
-            'DO UPDATE SET %(upsert_clause)s '
+            '%(do_statement)s '
             '%(returning_insert_id_sql)s' % {
                 'table': model._meta.db_table,
                 'columns': ','.join(dataframe.columns),
                 'values': get_insert_values_sql(self._cursor, dataframe.to_dict('split')['data']),
                 'unique_columns': ', '.join(get_unique_field_names(model)),
-                'upsert_clause': get_upsert_clause_sql(model),
+                'do_statement': do_statement,
                 'returning_insert_id_sql': self._connection.ops.return_insert_id()[0] % get_pk_column_name(model)
             }
         )
--- pandasio/validation/dataframes.py
+++ pandasio/validation/dataframes.py
@@ -68,6 +68,33 @@ class DataFrameSerializer(serializers.Serializer):
     def to_representation(self, instance):
         raise NotImplemented('`to_representation()` not implemented for `DataFrameSerializer`')
 
+    def is_valid(self, raise_exception=False):
+        assert not hasattr(self, 'restore_object'), (
+            'Serializer `%s.%s` has old-style version 2 `.restore_object()` '
+            'that is no longer compatible with REST framework 3. '
+            'Use the new-style `.create()` and `.update()` methods instead.' %
+            (self.__class__.__module__, self.__class__.__name__)
+        )
+
+        assert hasattr(self, 'initial_data'), (
+            'Cannot call `.is_valid()` as no `data=` keyword argument was '
+            'passed when instantiating the serializer instance.'
+        )
+
+        if not hasattr(self, '_validated_data'):
+            try:
+                self._validated_data = self.run_validation(self.initial_data)
+            except ValidationError as exc:
+                self._validated_data = pd.DataFrame()
+                self._errors = exc.detail
+            else:
+                self._errors = {}
+
+        if self._errors and raise_exception:
+            raise ValidationError(self.errors)
+
+        return not bool(self._errors)
+
     def save(self, using='default'):
         connection = connections[using]
         backend_module = get_dataframe_saver_backend(connection.settings_dict['ENGINE'])
