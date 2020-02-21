1fabc0f780c408d628f993a6fbdb17e8e78f1b00
NikitaZakharov <jake2000nik@gmail.com>
2020-02-21 19:37:08 +0200

    add returning_columns params for psql database saver

--- pandasio/db/postgresql.py
+++ pandasio/db/postgresql.py
@@ -1,12 +1,14 @@
 import io
 
 from pandasio.db.base import BaseDataFrameDatabaseSaver
-from pandasio.db.utils import get_unique_field_names, get_upsert_clause_sql, get_pk_column_name, get_insert_values_sql
+from pandasio.db.utils import get_unique_field_names, get_upsert_clause_sql, get_insert_values_sql
 
 
 class DataFrameDatabaseSaver(BaseDataFrameDatabaseSaver):
 
-    def save(self, dataframe, model):
+    def save(self, dataframe, model, returning_columns=None):
+        if returning_columns is not None:
+            return self.upsert(dataframe=dataframe, model=model, returning_columns=returning_columns)
         buffer = io.StringIO()
         dataframe.to_csv(buffer, sep='\t', header=False, index=False)
         buffer.seek(0)
@@ -16,20 +18,21 @@ class DataFrameDatabaseSaver(BaseDataFrameDatabaseSaver):
             print(e)
             self.upsert(dataframe=dataframe, model=model)
 
-    def upsert(self, dataframe, model):
+    def upsert(self, dataframe, model, returning_columns=None):
         upsert_clause = get_upsert_clause_sql(model)
         do_statement = 'DO UPDATE SET %s ' % upsert_clause if upsert_clause else 'DO NOTHING '
+        returning_statement = ('RETURNING %s' % ', '.join(returning_columns)) if returning_columns else ''
         self._cursor.execute(
             'INSERT INTO %(table)s (%(columns)s) '
             'VALUES %(values)s '
             'ON CONFLICT (%(unique_columns)s) '
             '%(do_statement)s '
-            '%(returning_insert_id_sql)s' % {
+            '%(returning_statement)s' % {
                 'table': model._meta.db_table,
                 'columns': ','.join(dataframe.columns),
                 'values': get_insert_values_sql(self._cursor, dataframe.to_dict('split')['data']),
                 'unique_columns': ', '.join(get_unique_field_names(model)),
                 'do_statement': do_statement,
-                'returning_insert_id_sql': self._connection.ops.return_insert_id()[0] % get_pk_column_name(model)
+                'returning_statement': returning_statement
             }
         )
